0f2d9196089b1e69c6abd8b6ef840467a0266b3f
Kuroshiel <alifianfikry@gmail.com>
2023-12-23 16:57:39 +0700

    Learn Default Select Channel

--- channel_test.go
+++ channel_test.go
@@ -112,7 +112,7 @@ func TestRangeChannel(t *testing.T) {
 
 // Select Channel
 
-func TestSeloctChannel(t *testing.T) {
+func TestSelectChannel(t *testing.T) {
 	channel1 := make(chan string)
 	channel2 := make(chan string)
 	defer close(channel1)
@@ -134,6 +134,40 @@ func TestSeloctChannel(t *testing.T) {
 		}
 
 		if counter == 2 {
+			fmt.Println("Selesail")
+			break
+		}
+	}
+}
+
+// Default Channel
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			fmt.Println("Selesail")
 			break
 		}
 	}
