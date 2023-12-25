71b0580283557db2707729c19cdffc328dd6e227
Kuroshiel <alifianfikry@gmail.com>
2023-12-25 13:01:23 +0700

    Learn sync.Mutex and fix it code race_codition_test.go

--- mutex_test.go
+++ mutex_test.go
@@ -2,19 +2,23 @@ package belajar_golang_goroutines
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
-// Race Condition
+// sync.Mutex
 
-func TestRaceCondition(t *testing.T) {
+func TestMutex(t *testing.T) {
 	x := 0
+	var mutex sync.Mutex
 
 	for i := 1; i <= 1000; i++ {
 		go func() {
 			for j := 1; j <= 100; j++ {
+				mutex.Lock()
 				x = x + 1
+				mutex.Unlock()
 			}
 		}()
 	}
